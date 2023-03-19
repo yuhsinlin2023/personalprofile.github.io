a718c92892c90c1eb46b71f278c78950b81a870a
yuhsinlin2023 <128274794+yuhsinlin2023@users.noreply.github.com>
2023-03-19 16:43:07 +0800

    version1

---
+++ personalfile/app.js
@@ -0,0 +1,10 @@
+let nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY == 0) {
+    nav.style.boxShadow = "";
+  } else {
+    nav.style.boxShadow = "0 10px 6px -6px #777";
+  }
+});
+// nav.style.boxShadow = ""如果scroll=0時，boxShadow等於空的
